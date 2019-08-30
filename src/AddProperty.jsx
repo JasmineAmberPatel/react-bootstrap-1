b25a3bd71f92493c8077241760b181cee06e5417
Jasmine Amber <jasmineamber.ja@gmail.com>
2019-08-30 16:21:01 +0100

    added some css form styling

--- src/AddProperty.jsx
+++ src/AddProperty.jsx
@@ -32,9 +32,7 @@ render() {
   return (
     <div className="AddProperty">
       <form onSubmit={this.handleAddProperty}>
-        <label> Property Description
-          <input name="title" value={this.state.fields.title} onChange={this.handleFieldChange} />
-        </label>
+        <label> Property Description <input name="title" value={this.state.fields.title} onChange={this.handleFieldChange} /></label>
         <label>Type of Property
           <select name="type" value={this.state.fields.type} onChange={this.handleFieldChange}>
             <option value="Flat">Flat</option>
@@ -45,7 +43,7 @@ render() {
             <option value="Bungalow">Bungalow</option>
           </select>
         </label>
-        <label>Location
+        <label> Location
           <select name="location" value={this.state.fields.location} onChange={this.handleFieldChange}>
             <option value="Manchester">Manchester</option>
             <option value="Leeds">Leeds</option>
@@ -53,19 +51,11 @@ render() {
             <option value="Liverpool">Liverpool</option>
           </select>
         </label>
-        <label> Bedrooms:
-          <input name="bedrooms" value={this.state.fields.bedrooms} onChange={this.handleFieldChange} />
-        </label>
-        <label> Bathrooms:
-          <input name="bathrooms" value={this.state.fields.bathrooms} onChange={this.handleFieldChange} />
-        </label>
-        <label> Price:
-          <input name="price" value={this.state.fields.price} onChange={this.handleFieldChange} />
-        </label>
-        <label> Email:
-          <input name="email" value={this.state.fields.email} onChange={this.handleFieldChange} />
-        </label> Submit Info:
-        <button type="submit"> Add </button>
+        <label> Bedrooms: <input name="bedrooms" value={this.state.fields.bedrooms} onChange={this.handleFieldChange} /></label>
+        <label> Bathrooms: <input name="bathrooms" value={this.state.fields.bathrooms} onChange={this.handleFieldChange} /></label>
+        <label> Price: <input name="price" value={this.state.fields.price} onChange={this.handleFieldChange} /></label>
+        <label> Email: <input name="email" value={this.state.fields.email} onChange={this.handleFieldChange} /></label>
+        <label> Submit Info: <button type="submit"> Add </button></label>
       </form>
     </div>
   );
